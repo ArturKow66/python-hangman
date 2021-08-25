61078aed7a161981b09b99bb0d8cdf1dcef0c34d
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2021-08-25 15:13:25 +0200

    Hangman - commit - finish

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,6 +1,7 @@
 import random
+import string
 
-is_running = True
+is_play = True
 guesses_counter = 8
 guesses_set = set()
 words_list = ['python', 'java', 'kotlin', 'javascript']
@@ -20,24 +21,33 @@ def change_in_guess(word, current_output, letter):
     return current_output
 
 
-guess_output = prepare_guess(word_to_guess)
-
 print('''H A N G M A N''')
-while guesses_counter != 0:
-    print(f'\n{guess_output}')
-    if guess_output == word_to_guess:
-        print('You guessed the word!')
-        print('You survived!')
+while is_play:
+    guess_output = prepare_guess(word_to_guess)
+    play = input('Type "play" to play the game, "exit" to quit: ')
+    if play == 'play':
+        while guesses_counter != 0:
+            print(f'\n{guess_output}')
+            if guess_output == word_to_guess:
+                print('You guessed the word!')
+                print('You survived!')
+                break
+            guess = input('Input a letter: ')
+            if len(guess) == 1:
+                if guess in string.ascii_lowercase:
+                    if guess in guesses_set:
+                        print("You've already guessed this letter")
+                    elif guess in set(word_to_guess):
+                        guess_output = change_in_guess(word_to_guess, guess_output, guess)
+                    else:
+                        guesses_counter -= 1
+                        print("That letter doesn't appear in the word")
+                    guesses_set.add(guess)
+                else:
+                    print('Please enter a lowercase English letter')
+            else:
+                print('You should input a single letter')
+        else:
+            print('You lost!')
+    elif play == 'exit':
         break
-    guess = input('Input a letter: ')
-    if guess in guesses_set:
-        guesses_counter -= 1
-        print('No improvements')
-    elif guess in set(word_to_guess):
-        guesses_set.add(guess)
-        guess_output = change_in_guess(word_to_guess, guess_output, guess)
-    else:
-        guesses_counter -= 1
-        print("That letter doesn't appear in the word")
-else:
-    print('You lost!')
